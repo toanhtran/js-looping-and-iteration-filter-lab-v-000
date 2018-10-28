aeb2d2ce4bce4f808e0309fae68fc4020afce2f6
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-28 18:39:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file
+function findMatching(list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  })
+}
