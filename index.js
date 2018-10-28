7adf6219cea19eef161b412402bbd1e7de1f830f
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-28 18:45:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function findMatching(list, name) {
 function fuzzyMatch (list, partialName) {
   let lengthOfName = partialName.length;
   return list.filter(function (driverName){
-    return driverName.slie(0, lengthOfName) === partialName;
-  })
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
